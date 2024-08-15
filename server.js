427cf19450ad318cf6d14d8c0761986a240fbeda
sashacheek <sasha@cheek.tv>
2024-08-15 17:02:00 -0600

    Add basic html and css

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  res.render('index');
 })
+
+app.post('/', function (req, res) {
+    res.render('index');
+  })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
