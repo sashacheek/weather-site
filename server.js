84f2557f32b77f5575d00bdbaa583d95b5e61d8e
sashacheek <sasha@cheek.tv>
2024-08-15 17:16:35 -0600

    Add weather API call

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
@@ -11,8 +15,21 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-    res.render('index');
-    console.log(req.body.city);
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
   })
 
 app.listen(3000, function () {
