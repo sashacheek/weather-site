7aed1fb99488300d7cffbcf867b610e3517a0979
sashacheek <sasha@cheek.tv>
2024-08-16 06:52:08 -0600

    Add weather icon based on weather info

---
+++ processing.js
@@ -0,0 +1,27 @@
+function processIcon(category){
+    switch(category) {
+        case "Thunderstorm":
+            return "/img/storm.svg"
+            break;
+        case "Drizzle":
+            return "/img/drizzle.svg"
+            break;
+        case "Rain":
+            return "/img/rainy.svg"
+            break;
+        case "Snow":
+            return "/img/snowy.svg"
+            break;
+        case "Clouds":
+            return "/img/cloudy.svg"
+            break;
+        case "Clear":
+            return "/img/sunny.svg"
+            break;
+        default:
+            return "/img/fog.svg"
+            break;
+    }
+}
+
+module.exports={processIcon}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const request = require('request');
+const processing = require('./processing');
 const app = express()
 
 require('dotenv').config();
@@ -13,7 +14,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('home');
+  res.render('home', {weather: null, icon: null, error: ''});
 })
 
 app.post('/', function (req, res) {
@@ -21,14 +22,20 @@ app.post('/', function (req, res) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
   request(url, function (err, response, body) {
       if(err){
-        res.render('home', {weather: null, error: 'Error, please try again'});
+        res.render('home', {weather: null, icon: null, error: 'Error, please try again'});
       } else {
         let weather = JSON.parse(body)
         if(weather.main == undefined){
-          res.render('home', {weather: null, error: 'Error, please try again'});
+          res.render('home', {weather: null, icon: null, error: 'Error, please try again'});
         } else {
+          let weatherId = weather.weather[0].main;
+          let weatherIcon = processing.processIcon(weatherId);
+          // get weather main id
+          // send to processing
+          // processing determines which image to render based on given id
+          // send image src here
           let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-          res.render('home', {weather: weatherText, error: null});
+          res.render('home', {weather: weatherText, icon: weatherIcon, error: null});
         }
       }
     });
