e979694dac74db00770dedcf520377ba520fe8bf
sashacheek <sasha@cheek.tv>
2024-08-16 20:46:05 -0600

    Add content to landing page

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
     let city = req.body.city;
+    
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
   request(url, function (err, response, body) {
       if(err){
