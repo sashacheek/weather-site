96248d6d5dfbaf36677f671cf020e7a64d27edcb
sashacheek <sasha@cheek.tv>
2024-09-06 08:17:57 -0600

    Improve view data processing

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+let viewData = { city: null, icon: null, temp: null, status: null, desc: null, feelsLike: null, humidity: null, wind: null, error: null, countries: constants.countries, states: constants.states }
+
 app.get('/', function (req, res) {
-  res.render('home', {city: null, icon: null, temp: null, status: null, desc: null, feelsLike: null, humidity: null, wind: null, error: null, countries: constants.countries});
+  res.render('home', viewData);
 })
 
 app.post('/', function (req, res) {
@@ -33,26 +35,32 @@ app.post('/', function (req, res) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
     // http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
   request(url, function (err, response, body) {
+
+      // if request fails
       if(err){
-        res.render('home', {city: null, icon: null, temp: null, status: null, desc: null, feelsLike: null, humidity: null, wind: null, error: 'Error, please try again', countries: constants.countries});
+        viewData.error = "Error, please try again"
+        res.render('home', viewData);
+
       } else {
         let weather = JSON.parse(body)
+
+        // if no result for location found
         if(weather.main == undefined){
-          res.render('home', {city: null, icon: null, temp: null, status: null, desc: null, feelsLike: null, humidity: null, wind: null, error: 'Error, please try again', countries: constants.countries});
-        } else {
-          // weather icon
+          viewData.error = "Error, please try again"
+          res.render('home', viewData);
+
+          // if successful
+          } else {
           let weatherId = weather.weather[0].main;
-          let weatherIcon = processing.processIcon(weatherId);
-          // weather city
-          let city = weather.name;
-          // weather temp
-          let temp = weather.main.temp;
-          let status = weather.weather[0].main;
-          let desc = weather.weather[0].description;
-          let feelsLike = weather.main.feels_like;
-          let humidity = weather.main.humidity;
-          let wind = weather.wind.speed;
-          res.render('home', {city: city, icon: weatherIcon,  temp: temp, status: status, desc: desc, feelsLike: feelsLike, humidity: humidity, wind: wind, error: null, countries: constants.countries});
+          viewData.icon = processing.processIcon(weatherId);
+          viewData.city = weather.name;
+          viewData.temp = weather.main.temp;
+          viewData.status = weather.weather[0].main;
+          viewData.desc = weather.weather[0].description;
+          viewData.feelsLike = weather.main.feels_like;
+          viewData.humidity = weather.main.humidity;
+          viewData.wind = weather.wind.speed;
+          res.render('home', viewData);
         }
       }
     });
